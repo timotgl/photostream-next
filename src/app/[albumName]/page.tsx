a20d28ad6ec09ace0f64c1ae4a9cd164cce350f1
Timo Taglieber <github@timotaglieber.de>
2023-12-18 16:36:00 +0100

    Parse album name from request url via middleware

--- src/app/[albumName]/page.tsx
+++ src/app/[albumName]/page.tsx
@@ -1,20 +1,12 @@
-import { headers, cookies } from "next/headers";
+import { headers } from "next/headers";
 
 export default function Album() {
   const headersList = headers();
-  const cookiesList = cookies();
-
+  const albumName = headersList.get('x-album-name');
   return (
     <div>
       <h1>This is the [albumName] route&apos;s page component!</h1>
-      <p>List of headers:</p>
-      <pre>
-        {JSON.stringify(headersList, null, 2 )}
-      </pre>
-      <p>List of cookies:</p>
-      <pre>
-        {JSON.stringify(cookiesList, null, 2 )}
-      </pre>
+      <p>Album name: {albumName}</p>
     </div>
   )
 };
