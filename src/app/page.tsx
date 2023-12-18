362314e3fbb19ba98e733c1cb08c6d2cc134537d
Timo Taglieber <github@timotaglieber.de>
2023-12-18 11:29:05 +0100

    Use Image component with one of the actual photos from timotaglieber.de

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,22 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 
+const imageSrc = "https://timotaglieber.de/photos/albums/highlights/3840/south-tufa.jpg";
+
 export default function Home() {
   return (
     <main className={styles.main}>
       <h1>Next.js dummy app</h1>
       <h2>Main app page</h2>
-      <p>Using the <code>&lt;Image&gt;</code> component:</p>
+      <p>
+        Using the <code>&lt;Image&gt;</code> component with an image from:<br/>
+        <code>{imageSrc}</code>
+      </p>
       <Image
-        src="/vercel.svg"
-        alt="Vercel Logo"
-        className={styles.vercelLogo}
-        width={100}
-        height={24}
+        src={imageSrc}
+        alt="South Tufa"
+        width={1024}
+        height={768}
         priority
       />
     </main>
