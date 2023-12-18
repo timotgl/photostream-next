2a87aa6498aa4f7ba37f5f7ed4d29cd9f068bdc7
Timo Taglieber <github@timotaglieber.de>
2023-12-18 11:46:08 +0100

    Test if commits pushed to main really deploy app to Vercel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,7 @@ export default function Home() {
     <main className={styles.main}>
       <h1>Next.js dummy app</h1>
       <h2>Main app page</h2>
+      <p>Pushing a commit to main on GitHub should deploy this app</p>
       <p>
         Using the <code>&lt;Image&gt;</code> component with an image from:<br/>
         <code>{imageSrc}</code>
