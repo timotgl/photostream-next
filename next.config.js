c23d7ac0b9ae9db8861ce901b0d578f26bd5d77b
Timo Taglieber <github@timotaglieber.de>
2023-12-18 11:19:13 +0100

    Configure timotaglieber.de as remote URL for image optimization API

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        // Configure Next.js Image Optimization API to access source images. Example:
+        // https://timotaglieber.de/photos/albums/highlights/3840/south-tufa.jpg
+        protocol: 'https',
+        hostname: 'timotaglieber.de',
+        port: '',
+        pathname: '/photos/albums/**',
+      },
+    ],
+  },
+}
 
 module.exports = nextConfig
