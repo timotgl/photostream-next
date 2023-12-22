312cace65e6b01c6ed3b0edc217f35506b10d4de
Timo Taglieber <github@timotaglieber.de>
2023-12-22 17:32:43 +0100

    Use google font from legacy app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Quattrocento_Sans } from 'next/font/google'
 import './globals.css'
 
-const inter = Inter({ subsets: ['latin'] })
+const font = Quattrocento_Sans({ subsets: ['latin'], weight: '400', style: 'normal' })
 
 export const metadata: Metadata = {
   title: 'Photos by Timo Taglieber',
@@ -16,7 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={font.className}>{children}</body>
     </html>
   )
 }
